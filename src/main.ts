7e080b68ff85d3107914f424b5e6755c540eb18f
Sriveer Neerukonda <Sriveer.NEERUKONDA@outlook.com>
2024-04-30 08:19:46 +0530

    feat:draw texture to screen

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import shaderSource from 'bundle-text:./shader.wgsl'
-// import littlerootImgPath from './img/littleroot.png';
+import littlerootImgPath from './img/littleroot.png';
 
 //Main method;
 (async () => {
     const canvas = document.getElementById('canvas-view') as HTMLCanvasElement;
-    // const littlerootBitmap: ImageBitmap = await asyncLoadImageFromUrl(littlerootImgPath);
+    const littlerootBitmap: ImageBitmap = await asyncLoadImageFromUrl(littlerootImgPath);
 
     //Check if webgpu can work on this machine
     const canvasContext: GPUCanvasContext | null = canvas.getContext("webgpu");
@@ -23,7 +23,7 @@ import shaderSource from 'bundle-text:./shader.wgsl'
     })
 
     //Create a position vertex buffer
-    const positionData = getTrianglePositionData();
+    const positionData = getQuadPosCoords();
     const positionBuffer = device.createBuffer({
         size: positionData.BYTES_PER_ELEMENT * positionData.length,
         usage: GPUBufferUsage.VERTEX,
@@ -32,15 +32,15 @@ import shaderSource from 'bundle-text:./shader.wgsl'
     new Float32Array(positionBuffer.getMappedRange()).set(positionData);
     positionBuffer.unmap();
 
-    //Create a color vertex buffer
-    const colorData = getTriangleRedColorData();
-    const colorBuffer = device.createBuffer({
-        size: colorData.length * colorData.BYTES_PER_ELEMENT,
+    //Create a texture coordinates buffer
+    const texCoordsData = getQuadTexCoords();
+    const texCoordsBuffer = device.createBuffer({
+        size: texCoordsData.length * Float32Array.BYTES_PER_ELEMENT,
         usage: GPUBufferUsage.VERTEX,
         mappedAtCreation: true
     });
-    new Float32Array(colorBuffer.getMappedRange()).set(colorData);
-    colorBuffer.unmap();
+    new Float32Array(texCoordsBuffer.getMappedRange()).set(texCoordsData);
+    texCoordsBuffer.unmap();
 
     //create shader module along with vertex and fragment state
     const shaderModule = device.createShaderModule({code: shaderSource});
@@ -49,7 +49,7 @@ import shaderSource from 'bundle-text:./shader.wgsl'
         entryPoint: 'vertexMain',
         buffers: [
             {
-                arrayStride: 2 * Float32Array.BYTES_PER_ELEMENT, //2 positions per vertex
+                arrayStride: 2 * Float32Array.BYTES_PER_ELEMENT, //2 values per one position entry
                 attributes: [
                     {
                         format: 'float32x2',
@@ -60,10 +60,10 @@ import shaderSource from 'bundle-text:./shader.wgsl'
                 stepMode: 'vertex'
             },
             {
-                arrayStride: 3 * Float32Array.BYTES_PER_ELEMENT, //3 colors per unit
+                arrayStride: 2 * Float32Array.BYTES_PER_ELEMENT, //2 values per one texture coordinate
                 attributes: [
                     {
-                        format: 'float32x3',
+                        format: 'float32x2',
                         offset: 0,
                         shaderLocation: 1
                     }
@@ -81,13 +81,62 @@ import shaderSource from 'bundle-text:./shader.wgsl'
             }
         ]
     };
-    
+
+    //Create texture
+    const texture = device.createTexture({
+        size: {width: littlerootBitmap.width, height: littlerootBitmap.height},
+        format: 'rgba8unorm',
+        usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST
+    });
+    device.queue.copyExternalImageToTexture({source: littlerootBitmap},{texture},[littlerootBitmap.width, littlerootBitmap.height]);
+
+    //Create sampler
+    const sampler = device.createSampler({
+        magFilter: "linear",
+        minFilter: "linear"
+    });
+
+    //Create a Bind Group
+    const bindGroupLayout: GPUBindGroupLayout = device.createBindGroupLayout({
+        entries: [
+            {
+                binding: 0,
+                visibility: GPUShaderStage.FRAGMENT,
+                sampler: {}
+            },
+            {
+                binding: 1,
+                visibility: GPUShaderStage.FRAGMENT,
+                texture: {}
+            }
+        ]
+    });
+
+    const bindGroup = device.createBindGroup({
+        layout: bindGroupLayout,
+        entries: [
+            {
+                binding: 0,
+                resource: sampler
+            },
+            {
+                binding: 1,
+                resource: texture.createView()
+            }
+        ]
+    });
+
+    //Create Pipeline layout
+    const pipelineLayout: GPUPipelineLayout = device.createPipelineLayout({
+        bindGroupLayouts: [bindGroupLayout]
+    });
+
     //Create the pipeline
     const pipeline = device.createRenderPipeline({
         vertex: vertexState,
         fragment: fragmentState,
         primitive: {topology: 'triangle-list'},
-        layout: 'auto'
+        layout: pipelineLayout
     });
 
     //Create the render pass encoder
@@ -105,9 +154,13 @@ import shaderSource from 'bundle-text:./shader.wgsl'
 
     //Set the pipeline and draw
     renderPassEncoder.setPipeline(pipeline);
+
+    //Bind everything and draw
     renderPassEncoder.setVertexBuffer(0,positionBuffer);
-    renderPassEncoder.setVertexBuffer(1,colorBuffer);
-    renderPassEncoder.draw(3);
+    renderPassEncoder.setVertexBuffer(1,texCoordsBuffer);
+    renderPassEncoder.setBindGroup(0,bindGroup);
+    renderPassEncoder.draw(6);
+   
     renderPassEncoder.end();
 
     //submit finished drawing to the screen
@@ -115,27 +168,35 @@ import shaderSource from 'bundle-text:./shader.wgsl'
 })();
 
 
-function getTrianglePositionData(): Float32Array {
+function getQuadPosCoords(): Float32Array {
     return new Float32Array([
         -0.5,-0.5, //bottom left
          0.5,-0.5, //bottom right
-         0.0, 0.5  //top center 
+         0.5, 0.5, //top right
+
+        -0.5, 0.5, //top left
+         0.5, 0.5, //top right
+        -0.5,-0.5, //bottom left
     ]);
 }
 
-function getTriangleRedColorData(): Float32Array {
+function getQuadTexCoords(): Float32Array {
     return new Float32Array([
-        1.0,0.0,0.0,
-        1.0,0.0,0.0,
-        1.0,0.0,0.0
+        0.0,1.0, //bottom left
+        1.0,1.0, //bottom right
+        1.0,0.0, //top right
+
+        0.0,0.0, //top left
+        1.0,0.0, //top right
+        0.0,1.0, //bottom left
     ]);
 }
 
-// function asyncLoadImageFromUrl(url: string): Promise<ImageBitmap> {
-//     return new Promise((resolve,reject) => {
-//         const image = document.createElement('img') as HTMLImageElement;
-//         image.onload = () => resolve(createImageBitmap(image));
-//         image.onerror = () => reject('unable to load image');
-//         image.src = url;
-//     });
-// }
+function asyncLoadImageFromUrl(url: string): Promise<ImageBitmap> {
+    return new Promise((resolve,reject) => {
+        const image = document.createElement('img') as HTMLImageElement;
+        image.onload = () => resolve(createImageBitmap(image));
+        image.onerror = () => reject('unable to load image');
+        image.src = url;
+    });
+}
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,17 +1,20 @@
 struct VertexOutput {
     @builtin(position) position: vec4f,
-    @location(0) color: vec4f
+    @location(0) texCoords: vec2f
 }
 
 @vertex
-fn vertexMain(@location(0) position: vec2f, @location(1) color: vec3f) -> VertexOutput {
+fn vertexMain(@location(0) position: vec2f, @location(1) texCoords: vec2f) -> VertexOutput {
     var out: VertexOutput;
     out.position = vec4f(position,0.0,1.0);
-    out.color = vec4f(color,1.0);
+    out.texCoords = texCoords;
     return out;
 }
 
+@group(0) @binding(0) var textureSampler: sampler;
+@group(0) @binding(1) var tex: texture_2d<f32>;
+
 @fragment
 fn fragmentMain(fragData: VertexOutput) -> @location(0) vec4f {
-    return fragData.color;
+    return textureSample(tex,textureSampler,fragData.texCoords);    
 }
